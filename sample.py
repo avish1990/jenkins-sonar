2e8bb8f22bac6a1a2181f62bb5454a1c6c01050d
avish1990 <avish1990@gmail.com>
2018-03-21 15:08:54 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,3 +5,8 @@ def handler(event, context):
     message = event['Records'][0]['Sns']['Message']
     Doc = 'avish-port'
     response=ssm.send_command(DocumentName=Doc,  Parameters={'Rule': [message.upper()]},  Targets=[{'Key': 'tag:env','Values': ['dev']}])
+    ssm = boto3.client('ssm')
+    message = event['Records'][0]['Sns']['Message']
+    Doc = 'avish-port'
+    response=ssm.send_command(DocumentName=Doc,  Parameters={'Rule': [message.upper()]},  Targets=[{'Key': 'tag:env','Values': ['dev']}])
+
