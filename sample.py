a0a56f897696521fe7144c552ba5019b6c407ca7
avish1990 <avish1990@gmail.com>
2018-03-21 14:52:25 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -13,18 +13,10 @@ def lambda_handler(event, context):
     documentName = 'AWS-RunShellScript'
     commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
     commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']
-    ssm = boto3.client('ssm')
-    message = event['Records'][0]['Sns']['Message']
-    documentName = 'AWS-RunShellScript'
-    commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
-    commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']
-    documentName = 'AWS-RunShellScript'
-    commandopen = ['iptables -I INPUT -p tcp --dport 8080 -j ACCEPT']
-    commandclose  = ['iptables -I INPUT -p tcp --dport 8080 -j DROP']	
-	ec2 = boto3.client('ec2')
+    ec2 = boto3.client('ec2')
     response = ec2.describe_instances()
       for r in response['Reservations']:
-      for instance in r['Instances']:  
+        for instance in r['Instances']:  
 	
         if message.lower() == 'start':
           status = ssm.send_command(DocumentName=documentName, TimeoutSeconds=timeout, Parameters={'commands': commandopen}, InstanceIds=[instance])
